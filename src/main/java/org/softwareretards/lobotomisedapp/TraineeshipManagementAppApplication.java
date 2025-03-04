d8f30b1fad6a8160d079decfbffbd38b096c79b1
Mario <mariosole9@gmail.com>
2025-03-04 21:15:04 +0200

    Initial commit

---
+++ src/main/java/org/softwareretards/lobotomisedapp/TraineeshipManagementAppApplication.java
@@ -0,0 +1,13 @@
+package org.softwareretards.lobotomisedapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeshipManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineeshipManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/softwareretards/lobotomisedapp/TraineeshipManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.softwareretards.lobotomisedapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeshipManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
