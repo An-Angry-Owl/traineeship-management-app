8dbbfc1123d35ccb23ae71d2aa12371b185ba76b
Mario <mariosole9@gmail.com>
2025-03-04 23:23:31 +0200

    forgot

--- databaseCreator.sql
+++ databaseCreator.sql
@@ -43,8 +43,6 @@ CREATE TABLE committee (
                            id BIGINT PRIMARY KEY AUTO_INCREMENT,
                            user_id BIGINT UNIQUE NOT NULL,
                            committee_name VARCHAR(100) NOT NULL,
-                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                           updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
