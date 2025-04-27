5e119aa1213cd449c36218554a05b644e6606baa
Mario <mariosole9@gmail.com>
2025-04-27 13:25:52 +0300

    Fixed a bug that prevented a student user from updating their password

--- src/main/java/org/softwareretards/lobotomisedapp/controller/StudentController.java
+++ src/main/java/org/softwareretards/lobotomisedapp/controller/StudentController.java
@@ -4,19 +4,23 @@ import org.softwareretards.lobotomisedapp.dto.LogbookEntryDto;
 import org.softwareretards.lobotomisedapp.dto.traineeship.TraineeshipApplicationDto;
 import org.softwareretards.lobotomisedapp.dto.user.StudentDto;
 import org.softwareretards.lobotomisedapp.service.StudentService;
+import org.softwareretards.lobotomisedapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class StudentController {
 
     private final StudentService studentService;
+    private final UserService userService;
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, UserService userService) {
         this.studentService = studentService;
+        this.userService = userService;
     }
 
     // US4: Profile Management
@@ -77,5 +81,21 @@ public class StudentController {
         );
         model.addAttribute("entry", savedEntry);
         return "students/logbook-confirmation";
+    }
+
+    @PostMapping("/students/{username}/change-password")
+    public String changePassword(
+            @PathVariable String username,
+            @RequestParam String currentPassword,
+            @RequestParam String newPassword,
+            @RequestParam String confirmPassword,
+            RedirectAttributes redirectAttributes) {
+        try {
+            userService.changePassword(username, currentPassword, newPassword, confirmPassword);
+            redirectAttributes.addFlashAttribute("success", "Password changed successfully!");
+        } catch (RuntimeException e) {
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+        }
+        return "redirect:/students/" + username + "/profile";
     }
 }
