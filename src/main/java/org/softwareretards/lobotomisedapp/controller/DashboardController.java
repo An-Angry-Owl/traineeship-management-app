e90ebe1ff93f61a744bf9b5079549aa73848d2ec
Mario <mariosole9@gmail.com>
2025-04-27 16:39:31 +0300

    Added a DashboardController to handle dashboard redirections

---
+++ src/main/java/org/softwareretards/lobotomisedapp/controller/DashboardController.java
@@ -0,0 +1,19 @@
+package org.softwareretards.lobotomisedapp.controller;
+
+import org.softwareretards.lobotomisedapp.entity.user.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String handleDashboardRedirect(@AuthenticationPrincipal User user) {
+        return switch (user.getRole()) {
+            case STUDENT -> "redirect:/students/" + user.getUsername() + "/dashboard";
+            case COMPANY -> "redirect:/companies/" + user.getUsername() + "/dashboard";
+            default -> "redirect:/";
+        };
+    }
+}
