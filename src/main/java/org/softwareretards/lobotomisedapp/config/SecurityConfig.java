1d5cb09655f506c1c4031e3351afa4c6884a44a5
Mario <mariosole9@gmail.com>
2025-04-27 20:19:58 +0300

    Undid the previous fix

--- src/main/java/org/softwareretards/lobotomisedapp/config/SecurityConfig.java
+++ src/main/java/org/softwareretards/lobotomisedapp/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/logout-success")
-                );
+                )
+                .csrf(csrf -> csrf.disable());
 
         return http.build();
     }
