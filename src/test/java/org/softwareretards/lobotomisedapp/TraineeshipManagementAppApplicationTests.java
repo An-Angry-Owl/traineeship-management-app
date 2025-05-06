ab2b1cb0255fdb8e954cb080feae3869caa76466
Mario <mariosole9@gmail.com>
2025-05-06 20:19:14 +0300

    .

--- src/test/java/org/softwareretards/lobotomisedapp/TraineeshipManagementAppApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package org.softwareretards.lobotomisedapp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TraineeshipManagementAppApplicationTests {
-
-}
